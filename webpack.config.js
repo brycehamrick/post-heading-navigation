129e7d020c5040e0522cd7775c4e6251cbc14fde
Bryce Hamrick <brycehamrick@gmail.com>
2024-11-04 13:05:59 -0600

    switched js to webpack

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'index.js',
+    },
+    module: {
+        noParse: /react\/jsx-runtime|react-dom/,
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            [ '@wordpress/babel-preset-default', { runtime: 'classic' } ]
+                        ],
+                    },
+                },
+            },
+        ],
+    },
+    externals: {
+        '@wordpress/blocks': [ 'wp', 'blocks' ],
+        '@wordpress/block-editor': [ 'wp', 'blockEditor' ],
+        '@wordpress/components': [ 'wp', 'components' ],
+        '@wordpress/element': [ 'wp', 'element' ],
+        'react': 'React',
+        'react-dom': 'ReactDOM',
+    },
+    resolve: {
+        alias: {
+            'react/jsx-runtime': false,
+            'react': false,
+            'react-dom': false,
+        },
+    },
+    plugins: [
+        new webpack.IgnorePlugin({ resourceRegExp: /^react$/ }),
+        new webpack.IgnorePlugin({ resourceRegExp: /^react-dom$/ }),
+    ],
+    mode: 'production',
+};
