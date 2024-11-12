1a8279fd72cf0d6519034aa07e09c61ce03c4fbc
Bryce Hamrick <brycehamrick@gmail.com>
2024-11-12 15:52:56 -0600

    save values

--- src/core-heading-modifications.js
+++ src/core-heading-modifications.js
@@ -1,3 +1,8 @@
+import { addFilter } from '@wordpress/hooks';
+import { InspectorControls } from '@wordpress/block-editor';
+import { PanelBody, TextControl, ToggleControl } from '@wordpress/components';
+import { Fragment } from '@wordpress/element';
+
 console.log("Script loaded for Core Heading Modifications");
 
 const initCoreHeadingModifications = () => {
@@ -35,33 +40,54 @@ const initCoreHeadingModifications = () => {
                     return (
                         <>
                             <BlockEdit {...props} />
-                            <wp.blockEditor.InspectorControls>
-                                <wp.components.PanelBody title="Navigation Settings">
-                                    <wp.components.TextControl
+                            <InspectorControls>
+                                <PanelBody title="Navigation Settings">
+                                    <TextControl
                                         label="Navigation Label"
                                         value={navigationLabel}
                                         onChange={(value) => setAttributes({ navigationLabel: value })}
                                         help="Custom label for this heading in the navigation menu."
                                     />
-                                    <wp.components.ToggleControl
+                                    <ToggleControl
                                         label="Exclude from Navigation"
                                         checked={excludeFromNavigation}
                                         onChange={(value) => setAttributes({ excludeFromNavigation: value })}
                                         help="Exclude this heading from the navigation menu."
                                     />
-                                </wp.components.PanelBody>
-                            </wp.blockEditor.InspectorControls>
+                                </PanelBody>
+                            </InspectorControls>
                         </>
                     );
                 };
             }, 'addHeadingInspectorControls');
 
+            const saveCustomAttributes = (extraProps, blockType, attributes) => {
+                if (blockType.name === 'core/heading') {
+                    if (attributes.navigationLabel) {
+                        extraProps['data-navigation-label'] = attributes.navigationLabel;
+                    }
+                    if (attributes.excludeFromNavigation) {
+                        extraProps['data-exclude-from-navigation'] = attributes.excludeFromNavigation;
+                    }
+                }
+                return extraProps;
+            };
+
+            // Register the filter to add inspector controls
             wp.hooks.addFilter(
                 'editor.BlockEdit',
                 'custom/heading-inspector-controls',
                 addHeadingInspectorControls
             );
 
+            // Register the filter to save custom attributes to block content
+            wp.hooks.addFilter(
+                'blocks.getSaveContent.extraProps',
+                'custom/save-heading-attributes',
+                saveCustomAttributes
+            );
+
+            // Unregister and re-register the modified core/heading block type
             wp.blocks.unregisterBlockType('core/heading');
             wp.blocks.registerBlockType('core/heading', { ...headingBlock, ...customHeadingSettings });
 
@@ -70,6 +96,7 @@ const initCoreHeadingModifications = () => {
             console.log("core/heading block not found");
         }
 
+        // Stop interval after modification
         clearInterval(checkReadyInterval);
     } else {
         console.log("Waiting for wp.blocks, wp.hooks, and wp.data...");
